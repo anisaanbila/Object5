2d99c9b02fb94575dfef8670f9e9bc6d7abb005d
anisaanbila <anisanbilaa@gmail.com>
2025-10-19 20:54:25 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -104,7 +104,7 @@ header[data-testid="stHeader"]{ display:none; }
 .card-small {
   padding: 5px 16px !important;   /* lebih tipis */
   border-radius: 14px !important;  /* opsional: biar tetap halus */
-  font-weight:700; font-size:2.2rem; margin-bottom:.2rem; color:#fff; margin-left:.2rem
+  font-weight:700; font-size:2.2rem; margin-bottom:.2rem; color:#fff; margin-left:0rem
 }
 
 /* ===== PROFILE CHIP (atas header) ===== */

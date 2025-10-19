e9a1e23baaacf746f7e305bd66c35e64eabd4ce9
anisaanbila <anisanbilaa@gmail.com>
2025-10-19 17:37:33 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -136,10 +136,10 @@ header[data-testid="stHeader"]{ display:none; }
 .stTabs [role="tab"] {
   display: flex;
   align-items: center;
-  gap: 1.1rem;                        /* jarak ikon ↔ teks */
+  gap: 0.8rem;                        /* jarak ikon ↔ teks */
   color: #E0E2FF !important;
   font-weight: 500 !important;
-  font-size: 1.5rem !important;       /* ukuran teks tab */
+  font-size: 1.3rem !important;       /* ukuran teks tab */
   line-height: 1.25 !important;
   padding: 0.8rem 1.6rem;
   border: none;

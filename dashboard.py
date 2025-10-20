eacd2bd993a055e6ce0c6c04fb8fd6baf05bbcf9
anisaanbila <anisanbilaa@gmail.com>
2025-10-20 23:02:14 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -573,7 +573,7 @@ with tab_docs:
         display:flex;
         align-items:center;
         gap:12px;
-        margin-top:-15px;
+        margin-top:-25px;
         margin-bottom:12px;
       }
       .icon-bubble{
